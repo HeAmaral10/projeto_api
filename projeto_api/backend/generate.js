bb803be555b70a38769ffb2c8a3ba6bd7b7ff056
HeAmaral10 <hefacebook100@gmail.com>
2025-04-24 15:44:53 -0300

    Geração de dados através da biblioteca faker js

---
+++ projeto_api/backend/generate.js
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker/locale/pt_BR';
+import { lodash } from 'lodash';
+import fs from 'fs';
+
+const peoples = lodash.times(50, function(n){
+    const firtName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    return {
+        id: n+1,
+        firtName: firtName,
+        lastName: lastName,
+        avatar: faker.image.avatar(),
+        address: faker.location.streetAddress(),
+        email: faker.internet.email({ firstName: firtName.toLowerCase, lastName: lastName.toLowerCase }),   
+    }
+});
+
+const data = {};
+data.peoples = peoples;
+fs.writeFile('db.json', JSON.stringify(data), (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Data written to file');
+});
